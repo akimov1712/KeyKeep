31ca273266d4b0461f0bb0a26b3f85f0105893d9
topbun <akimov1712@mail.ru>
2024-03-07 22:53:29 +0300

    Rename main to single activity

--- app/src/main/java/ru/topbun/keyKeep/SingleActivity.kt
+++ app/src/main/java/ru/topbun/keyKeep/SingleActivity.kt
@@ -3,9 +3,9 @@ package ru.topbun.keyKeep
 import androidx.appcompat.app.AppCompatActivity
     import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SingleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_single)
     }
 }
