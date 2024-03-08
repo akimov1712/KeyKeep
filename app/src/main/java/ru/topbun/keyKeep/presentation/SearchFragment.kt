d7fd956e38da98b96687b74a624320fabb1f5aa5
topbun <akimov1712@mail.ru>
2024-03-08 13:54:04 +0300

    Created layout for searchFragment

---
+++ app/src/main/java/ru/topbun/keyKeep/presentation/SearchFragment.kt
@@ -0,0 +1,11 @@
+package ru.topbun.keyKeep.presentation
+
+import dagger.hilt.android.AndroidEntryPoint
+import ru.topbun.keyKeep.databinding.FragmentSearchBinding
+import ru.topbun.keyKeep.presentation.base.BaseFragment
+
+@AndroidEntryPoint
+class SearchFragment : BaseFragment<FragmentSearchBinding>(FragmentSearchBinding::inflate) {
+
+
+}
