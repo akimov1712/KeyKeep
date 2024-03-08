d60ace2620a3622d777e20ce2041841a2e567d7b
topbun <akimov1712@mail.ru>
2024-03-08 10:42:51 +0300

    added fonts

--- app/src/main/java/ru/topbun/keyKeep/presentation/HomeFragment.kt
+++ app/src/main/java/ru/topbun/keyKeep/presentation/HomeFragment.kt
@@ -1,19 +1,11 @@
 package ru.topbun.keyKeep.presentation
 
-import androidx.lifecycle.ViewModelProvider
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import dagger.hilt.android.AndroidEntryPoint
-import ru.topbun.keyKeep.R
 import ru.topbun.keyKeep.databinding.FragmentHomeBinding
 import ru.topbun.keyKeep.presentation.base.BaseFragment
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
-
 
 
 }
